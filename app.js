bac3d2115f22545448f9d234ad671a734ce4ded8
Omachi John <omachi.john@yahoo.com>
2022-12-27 13:55:47 -0800

    create static html pages for display

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 const express = require("express");
+
+const app = express();
+
+app.listen(3000);
+
+app.get("/", (req, res) => {
+  res.send("<p>Home page</p>");
+});
+
+app.get("/about", (req, res) => {
+     res.send("<p>About page</p>");
+   });
