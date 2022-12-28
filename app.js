1eda80f56ac86a1a8cefceecd0336c1b0dcce88c
Omachi John <omachi.john@yahoo.com>
2022-12-27 20:25:32 -0800

    Dynamic data with ejs

--- app.js
+++ app.js
@@ -3,18 +3,36 @@ const express = require("express");
 const app = express();
 
 // configure view engine
-app.set("view engine", ejs);
+app.set("view engine", "ejs");
 
 app.listen(3000);
 
 app.get("/", (req, res) => {
-  res.render("index");
+  const blogs = [
+    {
+      title: "Yoshi finds eggs",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Mario finds stars",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "How to defeat bowser",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+  ];
+  res.render("index", { title: "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
-  res.render("about");
+  res.render("about", { title: "about" });
+});
+
+app.get("/blog/create", (req, res) => {
+  res.render("create", { title: "create a new blog" });
 });
 
 app.use((req, res) => {
-  res.status(400).render("404");
+  res.status(400).render("404", { title: "404" });
 });
